5d10ed27c52ef34cd362d2a0b5b668cbd9701393
alphanumeric75 <usegithub12@gmail.com>
2025-03-03 19:12:12 +0530

    Update sql_problem.sql

--- sql_problem.sql
+++ sql_problem.sql
@@ -9,3 +9,32 @@ from transactions
 group by category 
 order by freq desc 
 limit 1000; 
+
+''' using pyspark ''' 
+  
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, count
+
+spark = SparkSession.builder.appName("BigDataOptimization").getOrCreate()
+
+data = [(i, f"product_{i % 50000}", round(i * 0.1, 2)) for i in range(10000000)]  
+df = spark.createDataFrame(data, ["id", "category", "amount"])
+
+df.write.mode("overwrite").parquet("transactions.parquet")
+
+df.spark.CreateOrReplaceTempView('transactions') 
+
+spark.sql(
+	'''
+	select category, 
+		count(*) as freq 
+	from transactions 
+	group by category 
+	order by freq desc 
+	limit 1000 ;
+	'''  
+).show() 
+
+from pyspark.sql.functions import count 
+
+df.groupby('category').agg(count('category').alias('freq').orderBy('freq', ascending = False).show(1000) 
